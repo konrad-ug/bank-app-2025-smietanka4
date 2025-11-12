01fda91ad82680637672914fefe9f56843638d60
kwloczewski <k.wloczewski.261@studms.ug.edu.pl>
2025-11-12 15:24:39 +0100

    Feature 12 - zaciaganie kredytow

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -51,6 +51,70 @@ class TestAccount:
 
         assert account.history == [500, -300, -1]
         assert account.balance == 199.0
+
+    def test_loan_approved_three_incoming_transfers(self):
+        account = PersonalAccount("John", "Doe",'12345678911')
+        account.incoming_transfer(1000)
+        account.incoming_transfer(2000)    
+        account.incoming_transfer(2500)    
+
+        account.submit_for_loan(3000)
+
+        assert account.balance == 8500.0
+
+    def test_loan_approved_summed_five_transactions(self):
+        account = PersonalAccount("John", "Doe",'12345678911')
+        account.incoming_transfer(1000)
+        account.incoming_transfer(5000)
+        account.outgoing_transfer(500)
+        account.outgoing_transfer(1000)
+        account.incoming_transfer(2500)
+
+        account.submit_for_loan(3000)
+
+        assert account.balance == 10000.0
+
+    def test_loan_declined_last_three_transactions_werent_incoming_transfers(self):
+        account = PersonalAccount("John", "Doe",'12345678911')
+        account.incoming_transfer(1000)
+        account.outgoing_transfer(500)
+        account.incoming_transfer(1000)
+
+        account.submit_for_loan(3000)
+
+        assert account.balance == 1500.0
+
+    def test_loan_declined_unsufficient_amount_of_transactions(self):
+        account = PersonalAccount("John", "Doe",'12345678911')
+        account.incoming_transfer(1000)
+        account.incoming_transfer(1000)
+        account.incoming_transfer(1000)
+        account.outgoing_transfer(1000)
+
+        account.submit_for_loan(1000)
+
+        assert account.balance == 2000.0
+
+
+    def test_loan_declined_sufficient_amount_of_transactions_but_sum_is_less_than_loan(self):
+        account = PersonalAccount("John", "Doe",'12345678911')
+        account.incoming_transfer(1000)
+        account.incoming_transfer(1000)
+        account.incoming_transfer(1000)
+        account.outgoing_transfer(1000)
+        account.outgoing_transfer(1000)
+
+        account.submit_for_loan(2000)
+
+        assert account.balance == 1000.0
+
+
+
+
+
+    
+
+
         
 
 
