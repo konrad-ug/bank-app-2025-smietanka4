333f277cc28d6d56d4d35822ba454fceaea5e144
kwloczewski <k.wloczewski.261@studms.ug.edu.pl>
2025-11-12 15:55:48 +0100

    refaktor

--- tests/unit/test_loans.py
+++ tests/unit/test_loans.py
@@ -9,9 +9,7 @@ class TestAccount:
         return account
 
     def test_loan_approved_three_incoming_transfers(self, account):
-        account.incoming_transfer(1000)
-        account.incoming_transfer(2000)    
-        account.incoming_transfer(2500)    
+        account.history = [1000,2000,2500]
 
         result = account.submit_for_loan(3000)
 
@@ -20,11 +18,7 @@ class TestAccount:
         assert account.balance == 8500.0
 
     def test_loan_approved_summed_five_transactions(self, account):
-        account.incoming_transfer(1000)
-        account.incoming_transfer(5000)
-        account.outgoing_transfer(500)
-        account.outgoing_transfer(1000)
-        account.incoming_transfer(2500)
+        account.history = [1000,5000,-500,-1000,2500]
 
         result = account.submit_for_loan(3000)
 
@@ -33,9 +27,7 @@ class TestAccount:
         assert account.balance == 10000.0
 
     def test_loan_declined_last_three_transactions_werent_incoming_transfers(self, account):
-        account.incoming_transfer(1000)
-        account.outgoing_transfer(500)
-        account.incoming_transfer(1000)
+        account.history = [1000,-500,1000]
 
         result = account.submit_for_loan(3000)
 
@@ -44,10 +36,7 @@ class TestAccount:
         assert account.balance == 1500.0
 
     def test_loan_declined_unsufficient_amount_of_transactions(self, account):
-        account.incoming_transfer(1000)
-        account.incoming_transfer(1000)
-        account.incoming_transfer(1000)
-        account.outgoing_transfer(1000)
+        account.history = [1000,1000,1000,-1000]
 
         result = account.submit_for_loan(1000)
         assert not result
@@ -56,12 +45,8 @@ class TestAccount:
 
 
     def test_loan_declined_sufficient_amount_of_transactions_but_sum_is_less_than_loan(self, account):
-        account.incoming_transfer(1000)
-        account.incoming_transfer(1000)
-        account.incoming_transfer(1000)
-        account.outgoing_transfer(1000)
-        account.outgoing_transfer(1000)
-
+        account.history = [1000,1000,1000,-1000,-1000]
+        
         result = account.submit_for_loan(2000)
         assert not result
 
